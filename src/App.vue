87749f78e2450643a2195831c6d98bf5e9d09a6f
RyuJuWan <gogoxotj@naver.com>
2024-01-01 02:39:29 +0900

    add datebase

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
     <Header/>
-    <Home/>
+    <RouterView/>
     <Footer/>
 </template>
 
 <script>
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Home from "@/pages/Home";
 export default {
   name: 'App',
   components: {
     Header,
-    Footer,
-    Home
+    Footer
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
+import store from "@/scripts/store"
 import App from './App.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import Home from "@/pages/Home"
+import Login from "@/pages/Login.vue";
 
-createApp(App).mount('#app')
+const routes =[
+    {path: '/', component: Home},
+    {path: '/login', component:Login}
+]
+
+const router = createRouter({
+    history : createWebHistory(),
+    routes
+})
+createApp(App).use(store).use(router).mount('#app')
